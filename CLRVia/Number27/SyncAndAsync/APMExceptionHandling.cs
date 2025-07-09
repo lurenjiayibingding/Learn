3280ab5de8209363a0db4316296e316277e508cc
liujiguang <ljiguang@163.com>
2025-07-09 23:16:09 +0800

    APM中的异常处理

---
+++ CLRVia/Number27/SyncAndAsync/APMExceptionHandling.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace SyncAndAsync
+{
+    /// <summary>
+    /// APM中的异常处理
+    /// </summary>
+    internal class APMExceptionHandling
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public void CallWebRequest()
+        {
+            var webRequest = WebRequest.Create("http://0.0.0.0/");
+            webRequest.BeginGetResponse(ProcessWebResponse, webRequest);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="result"></param>
+        public void ProcessWebResponse(IAsyncResult result)
+        {
+            var webRequest = (WebRequest)result.AsyncState;
+            WebResponse response = null;
+            try
+            {
+                response = webRequest.EndGetResponse(result);
+                Console.WriteLine($"Web请求响应数据长度为{response.ContentLength}");
+            }
+            catch (WebException ex)
+            {
+                Console.WriteLine($"Web请求发生异常{JsonSerializer.Serialize(ex)}");
+            }
+            finally
+            {
+                if (response != null)
+                {
+                    response.Close();
+                }
+            }
+        }
+    }
+}
--- CLRVia/Number27/SyncAndAsync/Program.cs
+++ CLRVia/Number27/SyncAndAsync/Program.cs
@@ -6,25 +6,29 @@ namespace SyncAndAsync
     {
         static void Main(string[] args)
         {
-            var input = Console.ReadLine();
-            if (input == "1")
-            {
-                for (int i = 0; i < Environment.ProcessorCount; i++)
-                {
-                    new PipeServer();
-                }
-                Console.WriteLine("服务端命名管道已经启动");
-                Console.ReadLine();
-            }
-            else
-            {
-                for (int i = 0; i < 100; i++)
-                {
-                    new PipClient("localhost", i.ToString());
-                }
-            }
+            //var input = Console.ReadLine();
+            //if (input == "1")
+            //{
+            //    for (int i = 0; i < Environment.ProcessorCount; i++)
+            //    {
+            //        new PipeServer();
+            //    }
+            //    Console.WriteLine("服务端命名管道已经启动");
+            //    Console.ReadLine();
+            //}
+            //else
+            //{
+            //    for (int i = 0; i < 100; i++)
+            //    {
+            //        new PipClient("localhost", i.ToString());
+            //    }
+            //}
+
+            APMExceptionHandling apmException = new APMExceptionHandling();
+            apmException.CallWebRequest();
 
             Console.WriteLine("Hello, World!");
+            Console.ReadKey();
         }
     }
 }
