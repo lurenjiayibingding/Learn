ea86b4c892c18a8f75b0966b758cbff1f04808de
liujiguang <ljiguang@163.com>
2026-03-14 12:39:56 +0800

    天知道是啥

--- CLRVia/Number27/SyncAndAsync/CustomDefined/TAPPattern.cs
+++ CLRVia/Number27/SyncAndAsync/CustomDefined/TAPPattern.cs
@@ -13,26 +13,26 @@ namespace SyncAndAsync.CustomDefined
     {
         public async Task ReadFile(string path)
         {
-            using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate, FileAccess.ReadWrite))
-            {
-            }
+            //using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+            //{
+            //}
 
 
-            var stringCollection = new List<string>();
-            Parallel.ForEach(stringCollection, new ParallelOptions { MaxDegreeOfParallelism = Environment.ProcessorCount * 2 }, item =>
-            {
-                //写入到磁盘
-            });
+            //var stringCollection = new List<string>();
+            //Parallel.ForEach(stringCollection, new ParallelOptions { MaxDegreeOfParallelism = Environment.ProcessorCount * 2 }, item =>
+            //{
+            //    //写入到磁盘
+            //});
 
-            foreach (var item in stringCollection)
-            {
-                //写入到磁盘
-            }
+            //foreach (var item in stringCollection)
+            //{
+            //    //写入到磁盘
+            //}
 
-            Parallel.ForEachAsync(stringCollection, new ParallelOptions { MaxDegreeOfParallelism = Environment.ProcessorCount * 2 }, async item =>
-            {
-                //写入到磁盘
-            });
+            //Parallel.ForEachAsync(stringCollection, new ParallelOptions { MaxDegreeOfParallelism = Environment.ProcessorCount * 2 }, async item =>
+            //{
+            //    //写入到磁盘
+            //});
         }
     }
 }
