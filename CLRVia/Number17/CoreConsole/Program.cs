1ac89032ccfab7ae76f7c3d18f47a4f23139500d
liujiguang <ljiguang@163.com>
2026-09-24 16:24:16 +0800

    新增委托和反射的练习

--- CLRVia/Number17/CoreConsole/Program.cs
+++ CLRVia/Number17/CoreConsole/Program.cs
@@ -22,11 +22,15 @@ namespace CoreConsole
                 //delegateClass.SimpleInvoke1();
                 //delegateClass.SimpleInvoke2();
 
-                DelegateList dl = new DelegateList();
-                dl.CombineAndRemove();
+                //DelegateList dl = new DelegateList();
+                //dl.CombineAndRemove();
+                //dl.Run();
 
-                dl.Run();
-
+                #region 委托和反射
+                DeleteReflect dr = new DeleteReflect();
+                //dr.InvokeReflectDelegate(new string[] { "CoreConsole.Types.DeleteReflect+AddDelete,CoreConsole", "100", "200" });
+                dr.InvokeReflectDelegate(new string[] { "CoreConsole.Types.DeleteReflect+UpperDelegate,CoreConsole", "hello" });
+                #endregion
 
 
                 Console.WriteLine("Hello, World!");
---
+++ CLRVia/Number17/CoreConsole/Types/DeleteReflect.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Text;
+using System.Text;
+
+namespace CoreConsole.Types
+{
+    //public delegate int AddDelete(int a, int b);
+
+    //public delegate string UpperDelegate(string input);
+
+    /// <summary>
+    /// 委托与反射
+    /// </summary>
+    public class DeleteReflect
+    {
+
+        private delegate int AddDelete(int a, int b);
+
+        private delegate string UpperDelegate(string input);
+
+        private delegate DateTime ConverttDateTimeDelegate(string input);
+
+
+        /// <summary>
+        /// 调用通过反射创建的委托
+        /// </summary>
+        /// <param name="args"></param>
+        public void InvokeReflectDelegate(string[] args)
+        {
+            if (args == null || args.Length < 2)
+            {
+                throw new ArgumentException("Please provide two integer arguments.");
+            }
+
+            Delegate d = null;
+            object[] delArgs = new object[args.Length - 1];
+
+            var delType = Type.GetType(args[0]);
+            if (delType == typeof(AddDelete))
+            {
+                if (!Int32.TryParse(args[1], out int a) || !Int32.TryParse(args[2], out int b))
+                {
+                    throw new ArgumentException("Please provide valid integer arguments.");
+                }
+
+                delArgs[0] = a;
+                delArgs[1] = b;
+                d = Delegate.CreateDelegate(delType, this, nameof(AddMethod));
+            }
+            else
+            {
+                if (delType == typeof(UpperDelegate))
+                {
+                    delArgs[0] = args[1];
+                    d = Delegate.CreateDelegate(delType, this, nameof(UpperMethod));
+                }
+            }
+
+            if (d != null)
+            {
+                try
+                {
+                    var resul = d.DynamicInvoke(delArgs);
+                    Console.WriteLine($"结果的实际类型为：{resul.GetType().FullName}，结果的值为：{resul.ToString()}");
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine($"参数错误：{ex.Message}");
+                    throw;
+                }
+            }
+        }
+
+
+        public int AddMethod(int a, int b)
+        {
+            return a + b;
+        }
+
+        public string UpperMethod(string input)
+        {
+            return input.ToUpper();
+        }
+    }
+}
